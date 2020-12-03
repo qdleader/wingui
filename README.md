b76a38369e49952da56b54201912a05b5f4c146a
yk4545945@163.com <yk4545945@163.com>
2020-12-03 09:08:49 +0800

    button组件样式并打包配置

--- README.md
+++ README.md
@@ -21,3 +21,5 @@ npm run build
 ```
 
 ## 项目刚开始，有兴趣同学可以关注qdleader，回复Yui，添加我时候备注好Yui，加入到项目中来。
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 
 
 import Button, { ButtonType, ButtonSize} from './components/Button/button'
@@ -9,9 +8,10 @@ function App() {
     <div className="App">
      <Button disabled> Hello </Button>
      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello </Button>
-     <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Hello </Button>
-     <Button> Hello </Button>
-     <Button> Hello </Button>
+     <Button btnType={ButtonType.Link} target="_blank" href="http://www.baidu.com"> 百度 </Button>
+     <Button onClick={(e) => {e.preventDefault(); alert(7)}}> Hello </Button>
+    
+     <Button autoFocus> autoFocus </Button>
      <Button> Hello </Button>
     </div>
   );
